5541f1268b3d30347730493edd68664b03129fc1
Klemen Tušar <techouse@gmail.com>
2022-03-13 21:54:56 +0000

    :recycle: Refactor caching to AlfredWorkflow

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io' show Platform, exitCode, stdout;
+import 'dart:io' show exitCode, stdout;
 
 import 'package:alfred_workflow/alfred_workflow.dart'
     show
@@ -11,43 +11,14 @@ import 'package:algolia/algolia.dart' show AlgoliaQuerySnapshot;
 import 'package:args/args.dart' show ArgParser, ArgResults;
 import 'package:collection/collection.dart' show IterableExtension;
 import 'package:html_unescape/html_unescape.dart' show HtmlUnescape;
-import 'package:path/path.dart' show dirname;
-import 'package:stash/stash_api.dart'
-    show
-        Cache,
-        CacheEntryCreatedEvent,
-        CacheEntryEvictedEvent,
-        CacheEntryExpiredEvent,
-        CacheEntryRemovedEvent,
-        CacheEntryUpdatedEvent,
-        CreatedExpiryPolicy,
-        EventListenerMode,
-        LruEvictionPolicy,
-        CacheExtension;
-import 'package:stash_file/stash_file.dart'
-    show FileCacheStore, newFileLocalCacheStore;
 
 import 'src/constants/config.dart' show Config;
-import 'src/extensions/string_helpers.dart' show StringHelpers;
 import 'src/models/search_result.dart' show SearchResult;
 import 'src/services/algolia_search.dart' show AlgoliaSearch;
 
 final HtmlUnescape unescape = HtmlUnescape();
 
 final AlfredWorkflow workflow = AlfredWorkflow();
-
-final FileCacheStore store = newFileLocalCacheStore(
-  path: dirname(Platform.script.toFilePath()),
-  fromEncodable: (Map<String, dynamic> json) => AlfredItems.fromJson(json),
-);
-
-final Cache cache = store.cache<AlfredItems>(
-  name: 'query_cache',
-  maxEntries: 10,
-  eventListenerMode: EventListenerMode.synchronous,
-  evictionPolicy: const LruEvictionPolicy(),
-  expiryPolicy: const CreatedExpiryPolicy(Duration(minutes: 1)),
-);
 
 bool verbose = false;
 
@@ -75,19 +46,13 @@ void main(List<String> arguments) async {
     }
     final String queryString = query.join(' ').trim();
 
-    if (verbose) {
-      stdout.writeln('Query: "$queryString"');
-      _cacheVerbosity();
-    }
+    if (verbose) stdout.writeln('Query: "$queryString"');
 
     if (queryString.isEmpty) {
       _showPlaceholder();
     } else {
-      final AlfredItems? cachedItem =
-          await cache.get('${queryString}_${version}'.md5hex);
-      if (cachedItem != null) {
-        workflow.addItems(cachedItem.items);
-      } else {
+      workflow.cacheKey = '${queryString}_${version}';
+      if (await workflow.getItems() == null) {
         await _performSearch(queryString, version: version);
       }
     }
@@ -101,20 +66,6 @@ void main(List<String> arguments) async {
   } finally {
     workflow.run();
   }
-}
-
-void _cacheVerbosity() {
-  cache
-    ..on<CacheEntryCreatedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" added'))
-    ..on<CacheEntryUpdatedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.newEntry.key}" updated'))
-    ..on<CacheEntryRemovedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" removed'))
-    ..on<CacheEntryExpiredEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" expired'))
-    ..on<CacheEntryEvictedEvent<AlfredItems>>()
-        .listen((event) => print('Key "${event.entry.key}" evicted'));
 }
 
 void _showPlaceholder() {
@@ -134,7 +85,7 @@ Future<void> _performSearch(String query, {String? version}) async {
 
   if (snapshot.nbHits > 0) {
     final AlfredItems items = AlfredItems(
-      items: snapshot.hits
+      snapshot.hits
           .map((snapshot) => SearchResult.fromJson(snapshot.data))
           .map((result) {
         final int level = int.tryParse(result.type.substring(3)) ?? 0;
@@ -158,7 +109,6 @@ Future<void> _performSearch(String query, {String? version}) async {
         );
       }).toList(),
     );
-    cache.putIfAbsent('${query}_${version}'.md5hex, items);
     workflow.addItems(items.items);
   } else {
     final Uri url =
