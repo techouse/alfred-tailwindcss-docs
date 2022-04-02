881605cd8edfa9f947c1b749a6806ddd735b5ad8
Klemen Tušar <techouse@gmail.com>
2022-04-02 18:44:51 +0100

    :arrow_up: Updates

--- bin/src/constants/config.dart
+++ bin/src/constants/config.dart
@@ -1,7 +1,7 @@
 class Config {
   Config._();
 
-  static const String version = '2.0.8';
+  static const String version = '2.0.9';
   static final Uri githubRepositoryUrl =
       Uri.https('github.com', '/techouse/alfred-tailwindcss-docs');
   static const String algoliaApplicationId = 'KNPXZI5B0M';
